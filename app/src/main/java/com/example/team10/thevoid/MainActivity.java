86e17b125e09d0d2b8c449d1311da947ddcdfd68
kchang264 <kellyckc2017@gmail.com>
2019-04-07 11:55:29 -0700

    Doesn't crash anymore

--- app/src/main/java/com/example/team10/thevoid/MainActivity.java
+++ app/src/main/java/com/example/team10/thevoid/MainActivity.java
@@ -7,33 +7,19 @@ import android.graphics.Shader;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextPaint;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
-import ai.api.AIListener;
-//import ai.api.AIConfiguration;
-import ai.api.AIServiceContext;
-import ai.api.model.AIError;
-import ai.api.model.AIResponse;
-import ai.api.model.Result;
-import ai.api.android.AIConfiguration;
-import com.google.gson.JsonElement;
-import java.util.Map;
-import ai.api.android.AIService;
+public class MainActivity extends AppCompatActivity {
 
-
-public class MainActivity extends AppCompatActivity implements AIListener {
-    AIService aiService;
-    TextView t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        t = (TextView) findViewById(R.id.titleTextView);
+
         Button enterVoid = findViewById( R.id.enterButton );
         enterVoid.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,79 +27,13 @@ public class MainActivity extends AppCompatActivity implements AIListener {
                 openVoid();
             }
         });
-
-        final AIConfiguration config = new AIConfiguration("a607b2db7c04498da7670ea3c1c9c99a",
-                AIConfiguration.SupportedLanguages.English,
-                AIConfiguration.RecognitionEngine.System);
-        AIService aiService = AIService.getService(this, config);
-        aiService.setListener(this);
     }
 
-    public void buttonClicked(View view){
-        aiService.startListening();
-    }
     /**
      * Launches new activity
      */
     private void openVoid() {
         Intent intent = new Intent(this, VoidActivity.class);
         startActivity(intent);
-    }
-
-    /**
-     * Event fires when entire process finished successfully, and returns result object
-     *
-     * @param result the result object, contains server answer
-     */
-    @Override
-    public void onResult(AIResponse result) {
-        Log.d("anu", result.toString());
-        Result result1 = result.getResult();
-        t.setText(result1.getResolvedQuery()+"action: "+ result1.getAction());
-    }
-
-    /**
-     * Event fires if something going wrong while recognition or access to the AI server
-     *
-     * @param error the error description object
-     */
-    @Override
-    public void onError(AIError error) {
-
-    }
-
-    /**
-     * Event fires every time sound level changed. Use it to create visual feedback. There is no guarantee that this method will
-     * be called.
-     *
-     * @param level the new RMS dB value
-     */
-    @Override
-    public void onAudioLevel(float level) {
-
-    }
-
-    /**
-     * Event fires when recognition engine start listening
-     */
-    @Override
-    public void onListeningStarted() {
-
-    }
-
-    /**
-     * Event fires when recognition engine cancel listening
-     */
-    @Override
-    public void onListeningCanceled() {
-
-    }
-
-    /**
-     * Event fires when recognition engine finish listening
-     */
-    @Override
-    public void onListeningFinished() {
-
     }
 }
--- app/src/main/java/com/example/team10/thevoid/VoidActivity.java
+++ app/src/main/java/com/example/team10/thevoid/VoidActivity.java
@@ -2,28 +2,33 @@ package com.example.team10.thevoid;
 
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import ai.api.AIListener;
+import ai.api.android.AIService;
+import ai.api.android.AIConfiguration;
+import ai.api.model.AIError;
+import ai.api.model.AIResponse;
 
-public class VoidActivity extends AppCompatActivity {
+public class VoidActivity extends AppCompatActivity implements AIListener {
     LinearLayout layout;
     AnimationDrawable animationDrawable;
+    AIService aiService;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_void);
 
-        //setup exit button
+        /**
+         * Setup exit button
+         */
         Button exitButton = findViewById( R.id.exitButton );
         exitButton.setOnClickListener( new View.OnClickListener() {
             @Override
@@ -32,7 +37,9 @@ public class VoidActivity extends AppCompatActivity {
             }
         });
 
-        //setup background gradient animation
+        /**
+         * Setup background gradient transitions
+         */
         layout = (LinearLayout) findViewById(R.id.layout);
 
         animationDrawable = (AnimationDrawable) layout.getBackground();
@@ -40,9 +47,50 @@ public class VoidActivity extends AppCompatActivity {
         animationDrawable.setExitFadeDuration(4500);
         animationDrawable.start();
 
-        //setup vortex spinning
+        /**
+         * Start vortex animation
+         */
         ImageView vortexImage = findViewById(R.id.vortexImage);
         vortexImage.startAnimation(AnimationUtils.loadAnimation(this, R.anim.rotate));
+
+        /**
+         * Start THE VOID
+         */
+        final AIConfiguration config = new AIConfiguration("a607b2db7c04498da7670ea3c1c9c99a",
+                AIConfiguration.SupportedLanguages.English,
+                AIConfiguration.RecognitionEngine.System);
+        aiService = AIService.getService(this, config);
+        aiService.setListener(this);
+        aiService.startListening();
     }
 
+    @Override
+    public void onResult(AIResponse result) {
+
+    }
+
+    @Override
+    public void onError(AIError error) {
+
+    }
+
+    @Override
+    public void onAudioLevel(float level) {
+
+    }
+
+    @Override
+    public void onListeningStarted() {
+
+    }
+
+    @Override
+    public void onListeningCanceled() {
+
+    }
+
+    @Override
+    public void onListeningFinished() {
+
+    }
 }
