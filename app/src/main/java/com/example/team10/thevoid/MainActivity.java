d50ceb03814bddd2646b773f7672c1de2e7f6b94
soniascp <35044796+soniascp@users.noreply.github.com>
2019-04-07 10:31:47 -0700

    Tutorial
    Added implementations into gradle and edited mainActivity.

--- app/src/main/java/com/example/team10/thevoid/MainActivity.java
+++ app/src/main/java/com/example/team10/thevoid/MainActivity.java
@@ -13,7 +13,19 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import ai.api.AIListener;
+//import ai.api.AIConfiguration;
+import ai.api.AIServiceContext;
+import ai.api.model.AIError;
+import ai.api.model.AIResponse;
+import ai.api.model.Result;
+import ai.api.android.AIConfiguration;
+import com.google.gson.JsonElement;
+import java.util.Map;
+
+
+
+public class MainActivity extends AppCompatActivity implements AIListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +39,10 @@ public class MainActivity extends AppCompatActivity {
                 openVoid();
             }
         });
+
+        final AIConfiguration config = new AIConfiguration("CLIENT_ACCESS_TOKEN",
+                AIConfiguration.SupportedLanguages.English,
+                AIConfiguration.RecognitionEngine.System);
     }
 
     /**
@@ -35,5 +51,60 @@ public class MainActivity extends AppCompatActivity {
     private void openVoid() {
         Intent intent = new Intent(this, VoidActivity.class);
         startActivity(intent);
+    }
+
+    /**
+     * Event fires when entire process finished successfully, and returns result object
+     *
+     * @param result the result object, contains server answer
+     */
+    @Override
+    public void onResult(AIResponse result) {
+
+    }
+
+    /**
+     * Event fires if something going wrong while recognition or access to the AI server
+     *
+     * @param error the error description object
+     */
+    @Override
+    public void onError(AIError error) {
+
+    }
+
+    /**
+     * Event fires every time sound level changed. Use it to create visual feedback. There is no guarantee that this method will
+     * be called.
+     *
+     * @param level the new RMS dB value
+     */
+    @Override
+    public void onAudioLevel(float level) {
+
+    }
+
+    /**
+     * Event fires when recognition engine start listening
+     */
+    @Override
+    public void onListeningStarted() {
+
+    }
+
+    /**
+     * Event fires when recognition engine cancel listening
+     */
+    @Override
+    public void onListeningCanceled() {
+
+    }
+
+    /**
+     * Event fires when recognition engine finish listening
+     */
+    @Override
+    public void onListeningFinished() {
+
     }
 }
