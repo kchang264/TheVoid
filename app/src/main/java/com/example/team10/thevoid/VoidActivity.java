c42072563786e0da6ee9e59f3489e5575c13df1c
Jennifer <77jennifer77@users.noreply.github.com>
2019-04-06 23:00:45 -0700

    spinning

--- app/src/main/java/com/example/team10/thevoid/VoidActivity.java
+++ app/src/main/java/com/example/team10/thevoid/VoidActivity.java
@@ -7,7 +7,10 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 public class VoidActivity extends AppCompatActivity {
@@ -19,6 +22,7 @@ public class VoidActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_void);
 
+        //setup exit button
         Button exitButton = findViewById( R.id.exitButton );
         exitButton.setOnClickListener( new View.OnClickListener() {
             @Override
@@ -27,12 +31,17 @@ public class VoidActivity extends AppCompatActivity {
             }
         });
 
+        //setup background gradient animation
         layout = (LinearLayout) findViewById(R.id.layout);
 
         animationDrawable = (AnimationDrawable) layout.getBackground();
         animationDrawable.setEnterFadeDuration(4500);
         animationDrawable.setExitFadeDuration(4500);
         animationDrawable.start();
+
+        //setup vortex spinning
+        ImageView vortexImage = findViewById(R.id.vortexImage);
+        vortexImage.startAnimation(AnimationUtils.loadAnimation(this, R.anim.rotate));
     }
 
 }
